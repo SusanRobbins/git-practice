fa2706fade671e47869fb4d6f02d8dd96c4f505a
Susan Robbins <83petiterenard@gmail.com>
2025-07-27 15:22:01 -0500

    Polish code: clean structure, docstrings, PEP 8 spacing

--- text_utils.py
+++ text_utils.py
@@ -2,9 +2,11 @@ def word_count(text):
     """Count the number of words in the given text."""
     return len(text.split())
 
+
 def character_count(text):
     """Count the number of characters in the given text."""
     return len(text)
+
 
 def avg_word_length(text):
     """Calculate the average length of words in the given text."""
@@ -12,6 +14,7 @@ def avg_word_length(text):
     if not words:
         return 0
     return sum(len(word) for word in words) / len(words)
+
 
 if __name__ == "__main__":
     sample = "Git is amazing for version control!"
